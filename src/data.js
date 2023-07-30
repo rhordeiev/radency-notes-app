a8fca4c59abc06dc429c4bcad4bea1f3caad1bb5
ross3005 <roshordeiev@gmail.com>
2023-07-30 22:11:54 +0300

    feat: data store added

---
+++ src/data.js
@@ -0,0 +1,48 @@
+localStorage.currentId = 8;
+export let notes = [
+  {
+    id: 1,
+    name: "Note 1",
+    category: "Task",
+    content: "Need to check my application on 28/07/2023 and 29/07/2023",
+  },
+  {
+    id: 2,
+    name: "Note 2",
+    category: "Random Thought",
+    content: "I must try turkish tangerines",
+  },
+  {
+    id: 3,
+    name: "Note 3",
+    category: "Idea",
+    content:
+      "I need to follow code style guidelines to improve code readability",
+  },
+  {
+    id: 4,
+    name: "Note 4",
+    category: "Random Thought",
+    content: "I must try pink watermelon",
+  },
+  {
+    id: 5,
+    name: "Note 5",
+    category: "Idea",
+    content:
+      "I need to follow code style guidelines to improve code readability",
+  },
+  {
+    id: 6,
+    name: "Note 6",
+    category: "Task",
+    content: "Need to check my application on 01/09/2024 and 03/09/2024",
+  },
+  {
+    id: 7,
+    name: "Note 7",
+    category: "Random Thought",
+    content: "I must try yellow tomatos",
+  },
+];
+export let archived = [];
