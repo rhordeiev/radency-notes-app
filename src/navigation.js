3a8acbdc8bd735fdf3d1becdfcae1eb548625a66
ross3005 <roshordeiev@gmail.com>
2023-07-30 22:14:51 +0300

    feat: navigation functionality added

--- src/navigation.js
+++ src/navigation.js
@@ -12,3 +12,25 @@ getNavElements().forEach((navElement) => {
     setNavElementUnderlineWidth(navElement, "100%");
   });
 });
+
+const setTableMargin = (margin) =>
+  document
+    .querySelectorAll("table")
+    .forEach((table) => table.style.setProperty("--table-margin", margin));
+
+const getSectionWidth = () =>
+  document.querySelector("section").getBoundingClientRect().width;
+
+const mainTab = document.getElementById("main-tab");
+const summaryTab = document.getElementById("summary-tab");
+const archivedTab = document.getElementById("archived-tab");
+
+mainTab.addEventListener("click", () => {
+  setTableMargin("0px");
+});
+summaryTab.addEventListener("click", () => {
+  setTableMargin(`${-getSectionWidth()}px`);
+});
+archivedTab.addEventListener("click", () => {
+  setTableMargin(`${-2 * getSectionWidth()}px`);
+});
