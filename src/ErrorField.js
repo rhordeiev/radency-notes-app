af585b2ff4f865414dc8c58c4b6a0223f8d73052
ross3005 <roshordeiev@gmail.com>
2023-07-30 22:13:10 +0300

    feat: error field object added

---
+++ src/ErrorField.js
@@ -0,0 +1,12 @@
+export const ErrorField = {
+  Create: {
+    Name: "create-name-error",
+    Category: "create-category-error",
+    Content: "create-content-error",
+  },
+  Edit: {
+    Name: "edit-name-error",
+    Category: "edit-category-error",
+    Content: "edit-content-error",
+  },
+};
